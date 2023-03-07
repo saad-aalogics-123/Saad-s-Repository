2279fbcaa65fc073539b5cfed888f0c010f8ae8a
saad-aalogics-123 <saad.aalogics@gmail.com>
2023-03-07 11:46:19 +0500

    update

--- cypress/e2e/google.cy.js
+++ cypress/e2e/google.cy.js
@@ -3,7 +3,7 @@ import { google_search_class } from "./pom/gogle_search.cy";
 describe("suite 1", ()=>
 
 {   const lv=new google_search_class();
-    it("test case 1",()=>
+    it("test case 2",()=>
     
     {
 lv.visit_site()
